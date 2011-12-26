9fa96e92455677279f5dc8641e75ac683e5fe33f
Michael Müller <mich@elmueller.net>
2011-12-26 01:49:55 +0100

    fixed bug: now a unique filename for every pad is used

--- check-for-changes.sh
+++ check-for-changes.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
-node notify.js ${2} > tmp_new.html
+tmpname=`md5 -q ${2}`
+node notify.js ${2} > "$tmpname"_new.html
 
-if ! cmp -s tmp_new.html tmp.html; then
+# temporary local copy already exists?
+if [ ! -f "$tmpname".html ]
+then
+	echo "initializing..."
+	cp "$tmpname"_new.html "$tmpname".html
+fi
+
+if ! cmp -s "$tmpname"_new.html "$tmpname".html; then
 	# files are different
-	diff tmp_new.html tmp.html | mail -s "[Piratenpad] Changes" ${1}
+	diff "$tmpname"_new.html "$tmpname".html | mail -s "[Piratenpad] Changes" ${1}
 	echo "sending mail.."
 	
-	rm tmp.html
-	mv tmp_new.html tmp.html
+	rm "$tmpname".html
+	mv "$tmpname"_new.html "$tmpname".html
 else
 	echo "no changes..."
 fi
